6437041ccfd79f7526971ff0474f638709c88c6c
qazseee <qazseee@gmail.com>
2023-03-22 14:19:22 +0800

    Reformatted result

--- amazonscrape.py
+++ amazonscrape.py
@@ -14,17 +14,22 @@ firefox_options.add_argument('--headless')
 # driver = webdriver.Chrome(options=options)
 driver = webdriver.Firefox(options=firefox_options)
 
+
+# TODO: plot correlation between sentiment score and actual ratings
+
 def get_review(links):
     full_dict = dict()
     for brand in links:
         brand_dict = dict()
         for category in links[brand]:
-            category_dict = dict()
+            category_list = []
             print(category)
             for URL in links[brand][category]:
+                product_dict = dict()
                 driver.get(URL)
                 time.sleep(1)
                 product_name = driver.find_element(By.CLASS_NAME, 'a-size-large').text
+                product_rating = driver.find_element(By.CSS_SELECTOR, '.a-size-medium.a-color-base').text.split(' ')[0]
                 print(product_name)
                 review_list = []
 
@@ -32,14 +37,7 @@ def get_review(links):
                     print(driver.current_url)
                     page = driver.page_source
                     time.sleep(1)
-                    soup = BeautifulSoup(page, 'html.parser')
-                    reviews = soup.find_all('span', class_='review-text-content')
-                    for review in reviews:
-                        for stuff in review.contents:
-                            text = stuff.get_text()
-                            if text != '\n':
-                                review_list.append(stuff.get_text())
-                    print(len(review_list))
+                    get_review_list(page, review_list)
                     if len(review_list) >= 50:
                         break
                     try:
@@ -50,16 +48,34 @@ def get_review(links):
 
                 for text in review_list:
                     print(text)
-                category_dict[product_name] = review_list
-            brand_dict[category] = category_dict
+                product_dict["name"] = product_name
+                product_dict["rating"] = product_rating
+                product_dict["reviews"] = review_list
+                category_list.append(product_dict)
+            brand_dict[category] = category_list
         full_dict[brand] = brand_dict
-    with open("reviews.json", "w") as ff:
+    with open("reviews_extracted.json", "w") as ff:
         json.dump(full_dict, ff, indent=2)
     return full_dict
 
-with open('urls.json') as f:
-    URLS = json.load(f)
-full_dict = get_review(URLS)
-print(full_dict)
+
+def get_review_list(page, review_list):
+    soup = BeautifulSoup(page, 'html.parser')
+    reviews = soup.find_all('span', class_='review-text-content')
+    for review in reviews:
+        for stuff in review.contents:
+            text = stuff.get_text()
+            if text != '\n':
+                review_list.append(stuff.get_text())
+    print(len(review_list))
 
 
+def main():
+    with open('urls.json') as f:
+        URLS = json.load(f)
+    full_dict = get_review(URLS)
+    print(full_dict)
+
+
+if __name__ == '__main__':
+    main()
